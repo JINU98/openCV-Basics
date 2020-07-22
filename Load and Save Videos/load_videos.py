6a316be6f70f4dc29f0223a9c47ede0a8f6dd0e3
JINENDRA MALEKAR <jinendramalekar@gmail.com>
2020-07-22 21:59:51 +0530

    Lesson2

---
+++ Load and Save Videos/load_videos.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture(0)
+fourcc=cv2.VideoWriter_fourcc(*'XVID')
+out=cv2.VideoWriter('output.avi',fourcc,200,(640,480))
+while True:
+	ret,frame=cap.read(0)
+	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	out.write(gray)
+	cv2.imshow('frame',frame)
+	cv2.imshow('gary',gray)
+	if cv2.waitKey(50) & 0XFF == ord('q'):
+		break
+cap.release()
+out.release()
+cv2.destroyALLWindows()
